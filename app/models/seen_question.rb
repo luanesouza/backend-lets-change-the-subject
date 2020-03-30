9a7d59ad76e310d877f98269ad4ce201ed7aba5a
sylwiavargas <sylwia.vargas@gmail.com>
2020-03-29 23:41:56 -0400

    skipped defaults to false

--- app/models/seen_question.rb
+++ app/models/seen_question.rb
@@ -1,6 +1,13 @@
 class SeenQuestion < ApplicationRecord
+  after_initialize :init
+
   belongs_to :user
   belongs_to :question
 
   validates_presence_of :user_id, :question_id
+
+  def init
+    self.skipped = false if self.skipped.nil?          #will set the default value only if it's nil
+  end
+
 end
