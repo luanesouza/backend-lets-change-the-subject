067093ea9bd217da99921c959a8358a46c060db3
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-02 14:05:05 -0400

    update category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,34 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :remainingFriendsQs, :remainingCoworkersQs, :remainingPartnersQs
-
-  def remainingFriendsQs
-    get_questions("friends")
-  end
-
-  def remainingCoworkersQs
-    get_questions("coworkers")
-  end
-
-  def remainingPartnersQs
-    get_questions("partners")
-  end
-
-
-  def get_questions(cat)
-    questions = object.question_categories.map(&:question).map{|q| 
-      {
-        "id": q.id,
-        "spiciness": q.spiciness,
-        "additionalLink": q.additional_link,
-        "content": q.content
-      }}.shuffle
-    if questions.length > 50
-      return questions.sample(50)
-    else 
-      return questions
-    end
-  end
-
+  attributes :id, :name, :questions
 
   def questions
     questions = object.question_categories.map(&:question).map{|q| 
@@ -37,7 +8,12 @@ class CategorySerializer < ActiveModel::Serializer
       "spiciness": q.spiciness,
       "additionalLink": q.additional_link,
       "content": q.content
-    }}
+    }}.shuffle
+    if questions.length > 50
+      return questions.sample(50)
+    else 
+      return questions
+    end
   end 
 
 end
