9d8bc2987c5041f112d89bf93ff1247bdd07fcd9
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-02 15:26:36 -0400

    update categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::CategoriesController < ApplicationController
     def index
         begin
             @categories = Category.all
+            render json: @categories, status: 200
         rescue StandardError => e  
             render json: {"error": ("#{e.message}") }, status: 400
         end
