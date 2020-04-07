3ba448b46e49287544df12ef6f837df85eebb0ec
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-07 10:36:22 -0400

    Passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: { message: "Someone has already taken this username." }
     validates :email, presence: true, uniqueness: { message: "Someone has already used this email address." }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "There's something funky with your email address!" },  length: {maximum: 75}
+    validates :password, presence: true, length: { in: 8..32, message: "Your password should be between 8 and 32 characters long." }
+
 end
