df76beb397d19778893c47d8ce8ef40c513661a5
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-07 10:38:29 -0400

    Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_231957) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
